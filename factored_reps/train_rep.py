f31814f9a7756d57e513780476660c23bd20fcef
Cam Allen <csal@brown.edu>
2021-09-12 19:51:12 -0400

    Switch to 0-passenger experiences

--- factored_reps/train_rep.py
+++ factored_reps/train_rep.py
@@ -185,7 +185,7 @@ if args.walls != 'taxi':
     env.reset_agent()
 
 else:
-    tag = 'episodes-1000_steps-20_passengers-1'
+    tag = 'episodes-1000_steps-20_passengers-0'
     results_dir = os.path.join('results', 'taxi-experiences', tag)
     filename_pattern = os.path.join(results_dir, 'seed-*.pkl')
 
