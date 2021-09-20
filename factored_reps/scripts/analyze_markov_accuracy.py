f514998eb8df29f54a005311bc5531993df5c179
Cam Allen <csal@brown.edu>
2021-09-20 18:10:02 -0400

    Update analyze_markov script

--- factored_reps/scripts/analyze_markov_accuracy.py
+++ factored_reps/scripts/analyze_markov_accuracy.py
@@ -25,6 +25,10 @@ args.latent_dims = 5
 args.markov_dims = 5
 args.other_args = []
 
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+print('device: {}'.format(device))
+
+
 params = load_hyperparams_and_inject_args(args)
 args = argparse.Namespace(**params)
 
@@ -70,19 +74,24 @@ goals = extract_array(experiences, 'goal')
 
 #%% ------------------ Load model ------------------
 def torchify(x):
-    return torch.as_tensor(np.moveaxis(x, -1, 0)).float().unsqueeze(0)
+    return torch.as_tensor(np.moveaxis(x, -1, 0)).float().unsqueeze(0).to(device)
 
 x = torchify(obs[0])
 xp = torchify(next_obs[0])
-fnet = FeatureNet(args, n_actions=5, input_shape=x.squeeze(0).shape, latent_dims=args.latent_dims)
-fnet.load(model_file, to='cpu')
+fnet = FeatureNet(args, n_actions=5, input_shape=x.squeeze(0).shape, latent_dims=args.latent_dims, device=device)
+fnet.load(model_file, to=device)
 
 #%%
-n_correct = 0
-for x, a, xp in tqdm(zip(obs, actions, next_obs), total=len(states)):
-    a_hat = fnet.predict_a(torchify(x), torchify(xp)).numpy()
-    if a == a_hat:
-        n_correct += 1
-n_correct / len(states)
+n_training = len(states)//2
+n_test = 2000
 
-print(n_correct, 'correct out of', len(states), 'total = {}%'.format(100*n_correct/len(states)))
+a_hat = fnet.predict_a(torchify(x), torchify(xp)).numpy()
+
+n_train_correct = (actions[:n_training] == a_hat[:n_training]).sum()
+n_test_correct = (actions[-n_test:] == a_hat[-n_test:]).sum()
+
+train_accuracy = 100 * n_train_correct / n_training
+test_accuracy = 100 * n_test_correct / n_test
+
+print('Training:', n_train_correct, 'correct out of', n_training, 'total = {}%'.format(train_accuracy))
+print('Training:', n_test_correct, 'correct out of', n_test, 'total = {}%'.format(test_accuracy))
