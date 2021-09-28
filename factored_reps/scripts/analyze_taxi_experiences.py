e1db06a394423a1af9909f1eedba984183de2f37
Cam Allen <csal@brown.edu>
2021-09-27 23:31:34 -0400

    Fix analyze_taxi_experiences script to handle 0-passenger case

--- factored_reps/scripts/analyze_taxi_experiences.py
+++ factored_reps/scripts/analyze_taxi_experiences.py
@@ -81,28 +81,31 @@ np.min(counts*25/len(states))
 np.max(counts*25/len(states))
 
 #%%
-counts = plt.hist2d(states[:, passenger_col], states[:, passenger_row], bins=5)[0]
-plt.colorbar()
-plt.gca().invert_yaxis()
-plt.title('passenger location')
-plt.show()
-np.min(counts*25/len(states))
-np.max(counts*25/len(states))
+if args.n_passengers > 0:
+    counts = plt.hist2d(states[:, passenger_col], states[:, passenger_row], bins=5)[0]
+    plt.colorbar()
+    plt.gca().invert_yaxis()
+    plt.title('passenger location')
+    plt.show()
+    np.min(counts*25/len(states))
+    np.max(counts*25/len(states))
 
 #%%
-counts = plt.hist2d(goals[:, goal_col], goals[:, goal_row], bins=5)[0]
-plt.colorbar()
-plt.gca().invert_yaxis()
-plt.title('goal')
-plt.show()
-np.min(counts[counts>0]*4/len(states))
-np.max(counts*4/len(states))
+if args.n_passengers > 0:
+    counts = plt.hist2d(goals[:, goal_col], goals[:, goal_row], bins=5)[0]
+    plt.colorbar()
+    plt.gca().invert_yaxis()
+    plt.title('goal')
+    plt.show()
+    np.min(counts[counts>0]*4/len(states))
+    np.max(counts*4/len(states))
 
 #%%
-sns.histplot(states[:, 4], discrete=True)
-np.histogram(states[:, 4], bins=2)[0]*2/len(states)
-plt.title('passenger in taxi?')
-plt.show()
+if args.n_passengers > 0:
+    sns.histplot(states[:, in_taxi], discrete=True)
+    np.histogram(states[:, in_taxi], bins=2)[0]*2/len(states)
+    plt.title('passenger in taxi?')
+    plt.show()
 
 #%%
 sns.histplot(actions[:], discrete=True)
@@ -118,29 +121,34 @@ taxi_moved = (different_taxi_col ^ different_taxi_row)
 taxi_moved.sum()/len(states)
 
 #%% Passenger moved
-different_passenger_col = states[:, passenger_col] != next_states[:, passenger_col]
-different_passenger_row = states[:, passenger_row] != next_states[:, passenger_row]
-passenger_moved = (different_passenger_col ^ different_passenger_row)
-passenger_moved.sum()/len(states)
+if args.n_passengers > 0:
+    different_passenger_col = states[:, passenger_col] != next_states[:, passenger_col]
+    different_passenger_row = states[:, passenger_row] != next_states[:, passenger_row]
+    passenger_moved = (different_passenger_col ^ different_passenger_row)
+    passenger_moved.sum()/len(states)
 
 #%% Pickup/dropoff
-pickup_or_dropoff = (states[:, in_taxi] != next_states[:, in_taxi])
-pickup_or_dropoff.sum()/len(states)
+if args.n_passengers > 0:
+    pickup_or_dropoff = (states[:, in_taxi] != next_states[:, in_taxi])
+    pickup_or_dropoff.sum()/len(states)
 
 #%% Taxi at passenger
-taxi_at_passenger = ((states[:, passenger_col] == states[:, taxi_col]) & (states[:, passenger_row] == states[:, taxi_row]))
-taxi_at_passenger.sum()/len(states)
+if args.n_passengers > 0:
+    taxi_at_passenger = ((states[:, passenger_col] == states[:, taxi_col]) & (states[:, passenger_row] == states[:, taxi_row]))
+    taxi_at_passenger.sum()/len(states)
 
 #%% Taxi L1 distance from passenger
-row_dist = np.abs(states[:, taxi_row] - states[:, passenger_row])
-col_dist = np.abs(states[:, taxi_col] - states[:, passenger_col])
-taxi_dist = (row_dist + col_dist)
-sns.histplot(taxi_dist, discrete=True)
-plt.title('Taxi L1 distance from passenger')
-plt.show()
+if args.n_passengers > 0:
+    row_dist = np.abs(states[:, taxi_row] - states[:, passenger_row])
+    col_dist = np.abs(states[:, taxi_col] - states[:, passenger_col])
+    taxi_dist = (row_dist + col_dist)
+    sns.histplot(taxi_dist, discrete=True)
+    plt.title('Taxi L1 distance from passenger')
+    plt.show()
 
 #%% Goal frequencies
-np.unique(goals, axis=0, return_counts=True)[1]/len(states)
+if args.n_passengers > 0:
+    np.unique(goals, axis=0, return_counts=True)[1]/len(states)
 
 #%% Episode frequencies
 episode_length = int(len(states)/n_episodes)
@@ -156,39 +164,43 @@ plt.title('Taxi L1 distance traveled')
 plt.show()
 
 #%% Passenger L1 distance traveled
-ep_start_states = states[0::episode_length]
-ep_end_states = states[episode_length-1::episode_length]
-row_dist = np.abs(ep_start_states[:, passenger_row] - ep_end_states[:, passenger_row])
-col_dist = np.abs(ep_start_states[:, passenger_col] - ep_end_states[:, passenger_col])
-passenger_dist = (row_dist + col_dist).astype(int)
-sns.histplot(passenger_dist, discrete=True)
-plt.title('Passenger L1 distance traveled')
-plt.show()
+if args.n_passengers > 0:
+    ep_start_states = states[0::episode_length]
+    ep_end_states = states[episode_length-1::episode_length]
+    row_dist = np.abs(ep_start_states[:, passenger_row] - ep_end_states[:, passenger_row])
+    col_dist = np.abs(ep_start_states[:, passenger_col] - ep_end_states[:, passenger_col])
+    passenger_dist = (row_dist + col_dist).astype(int)
+    sns.histplot(passenger_dist, discrete=True)
+    plt.title('Passenger L1 distance traveled')
+    plt.show()
 
 #%% Analyze shuffled samples
 idx = np.random.permutation(len(states))
 shuffled_goals = np.copy(goals)[idx]
 shuffled_next_states = np.copy(next_states)[idx]
 
-goal_matches = np.all(shuffled_goals == goals, axis=1)
+if args.n_passengers > 0:
+    goal_matches = np.all(shuffled_goals == goals, axis=1)
 
 taxi_row_delta = np.abs(states[:, taxi_row] - shuffled_next_states[:, taxi_row])
 taxi_col_delta = np.abs(states[:, taxi_col] - shuffled_next_states[:, taxi_col])
 taxi_delta = (taxi_row_delta + taxi_col_delta).astype(int)
 valid_taxi_delta = (taxi_delta <= 1)
 
-passenger_row_delta = np.abs(states[:, passenger_row] - shuffled_next_states[:, passenger_row])
-passenger_col_delta = np.abs(states[:, passenger_col] - shuffled_next_states[:, passenger_col])
-passenger_delta = (passenger_row_delta + passenger_col_delta).astype(int)
-valid_passenger_delta = (passenger_delta <= 1)
+if args.n_passengers > 0:
+    passenger_row_delta = np.abs(states[:, passenger_row] - shuffled_next_states[:, passenger_row])
+    passenger_col_delta = np.abs(states[:, passenger_col] - shuffled_next_states[:, passenger_col])
+    passenger_delta = (passenger_row_delta + passenger_col_delta).astype(int)
+    valid_passenger_delta = (passenger_delta <= 1)
 
-valid_delta = valid_taxi_delta & valid_passenger_delta
+    valid_delta = valid_taxi_delta & valid_passenger_delta
 
-goal_matches.sum()/len(states)
 valid_taxi_delta.sum()/len(states)
-valid_passenger_delta.sum()/len(states)
-valid_delta.sum()/len(states)
-(goal_matches & valid_delta).sum()/len(states)
+if args.n_passengers > 0:
+    valid_passenger_delta.sum()/len(states)
+    valid_delta.sum()/len(states)
+    goal_matches.sum()/len(states)
+    (goal_matches & valid_delta).sum()/len(states)
 pass
 
 # -----------------------------------------------------------------
@@ -196,5 +208,8 @@ pass
 #%% Check that observations look reasonable
 
 for i in range(1):
-    plt.imshow(obs[i], cmap='gray')
+    if obs[i].shape[-1] == 3:
+        plt.imshow(obs[i])
+    else:
+        plt.imshow(obs[i], cmap='gray')
     plt.show()
