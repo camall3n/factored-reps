c1f4ee5bc8dd5e0120fa735a2f06be4ccfa11fea
Cam Allen <csal@brown.edu>
2022-04-04 20:11:56 -0400

    Add save/load functionality to ReplayMemory

--- factored_reps/agents/replaymemory.py
+++ factored_reps/agents/replaymemory.py
@@ -1,5 +1,6 @@
 from collections import defaultdict
-import random
+import json
+import os
 
 import numpy as np
 
@@ -61,3 +62,48 @@ class ReplayMemory:
                 items = self.on_retrieve['*'](items)
             items = self.on_retrieve[key](items)
         return items
+
+    def save(self, directory, filename='replaymemory', extension='.json'):
+        if extension != '.json':
+            raise NotImplementedError
+        fields = sorted(list(self.fields))
+
+        data = [{key: serialize_np(value)
+                 for key, value in experience.items()} for experience in self.memory]
+        archive = {
+            'capacity': self.capacity,
+            'position': self.position,
+            'fields': fields,
+            'memory': data,
+        }
+        os.makedirs(directory, exist_ok=True)
+        filepath = os.path.join(directory, filename + extension)
+        with open(filepath, 'w') as fp:
+            json.dump(archive, fp)
+
+    def load(self, filepath):
+        with open(filepath, 'r') as fp:
+            archive = json.load(fp)
+        self.capacity = int(archive['capacity'])
+        self.position = int(archive['position'])
+        self.fields = set(archive['fields'])
+        self.memory = [{
+            key: unserialize_np(value, dtype)
+            for (key, (value, dtype)) in experience.items()
+        } for experience in archive['memory']]
+
+def serialize_np(value):
+    output = value
+    dtype = None
+    try:
+        output = value.tolist()
+        dtype = str(value.dtype)
+    except AttributeError:
+        pass
+    return output, dtype
+
+def unserialize_np(value, dtype):
+    output = value
+    if dtype is not None:
+        output = np.asarray(value, dtype=dtype)
+    return output
