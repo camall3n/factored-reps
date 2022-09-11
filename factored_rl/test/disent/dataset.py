20965a4e1ea4647430bc9c68b8496de01b1a8fbf
Cam Allen <csal@brown.edu>
2022-09-11 18:31:09 -0400

    Switch test to use DCI score with ObservationPermutationWrapper

--- factored_rl/test/disent/dataset.py
+++ factored_rl/test/disent/dataset.py
@@ -1,28 +1,39 @@
 import pytest
 
 import numpy
+import gym
 
 from disent.dataset.data import GymEnvData
 from disent.dataset import DisentDataset
-from disent.frameworks.ae import Ae
-from disent.model.ae import DecoderIdentity, EncoderIdentity
 
-from disent.model import AutoEncoder
 from disent.dataset.sampling import SingleSampler
-from disent.dataset.transform import ToImgTensorF32
-from disent.metrics import metric_mig
+from disent.metrics import metric_dci
 
 from visgrid.envs.components import Grid
 from visgrid.envs import GridworldEnv
-from visgrid.wrappers import NormalizedFloatWrapper, NoiseWrapper, ImageFrom1DWrapper
+from visgrid.wrappers import NormalizedFloatWrapper, NoiseWrapper
 
-from factored_rl.wrappers import FactorPermutationWrapper
+from factored_rl.wrappers import ObservationPermutationWrapper, FactorPermutationWrapper
+
+@pytest.fixture
+def ring_env_data():
+    grid = Grid.generate_ring(10, 10, 8)
+    env = GridworldEnv.from_grid(grid, hidden_goal=True, should_render=False)
+    data = GymEnvData(env)
+    return data
+
+def test_sample_single_valid_position(ring_env_data):
+    pos = ring_env_data.sample_factors()
+    assert ring_env_data.env.is_valid_pos(pos)
+
+def test_sample_multiple_valid_positions(ring_env_data):
+    positions = ring_env_data.sample_factors(size=100)
+    for pos in positions:
+        assert ring_env_data.env.is_valid_pos(pos)
 
 @pytest.fixture
 def unwrapped_env_data():
-    grid = Grid.generate_ring(10, 10, 8)
-    env = GridworldEnv.from_grid(grid, hidden_goal=True, should_render=False)
-    env = ImageFrom1DWrapper(env)
+    env = GridworldEnv(10, 10, hidden_goal=True, should_render=False)
     data = GymEnvData(env)
     return data
 
@@ -30,25 +41,14 @@ def test_unwrapped_shapes(unwrapped_env_data):
     state = unwrapped_env_data.sample_factors()
     idx = unwrapped_env_data.pos_to_idx(state)
     obs = unwrapped_env_data._get_observation(idx)
-    assert obs.shape == (2, 1, 1)
-
-def test_sample_single_valid_position(unwrapped_env_data):
-    pos = unwrapped_env_data.sample_factors()
-    assert unwrapped_env_data.env.is_valid_pos(pos)
-
-def test_sample_multiple_valid_positions(unwrapped_env_data):
-    positions = unwrapped_env_data.sample_factors(size=100)
-    for pos in positions:
-        assert unwrapped_env_data.env.is_valid_pos(pos)
+    assert obs.shape == (2, )
 
 @pytest.fixture
 def wrapped_env_data():
-    grid = Grid.generate_ring(10, 10, 8)
-    env = GridworldEnv.from_grid(grid, hidden_goal=True, should_render=False)
-    env = FactorPermutationWrapper(env)
+    env = GridworldEnv(10, 10, hidden_goal=True, should_render=False)
+    env = ObservationPermutationWrapper(env)
     env = NormalizedFloatWrapper(env)
     env = NoiseWrapper(env)
-    env = ImageFrom1DWrapper(env)
     data = GymEnvData(env)
     return data
 
@@ -56,17 +56,15 @@ def test_wrapped_shapes(wrapped_env_data):
     state = wrapped_env_data.sample_factors()
     idx = wrapped_env_data.pos_to_idx(state)
     obs = wrapped_env_data._get_observation(idx)
-    assert obs.shape == (2, 1, 1)
+    assert obs.shape == (2, )
 
-def test_compute_mig(unwrapped_env_data, wrapped_env_data):
-    def get_mig(data):
-        module = Ae(model=AutoEncoder(
-            encoder=EncoderIdentity(x_shape=data.x_shape),
-            decoder=DecoderIdentity(x_shape=data.x_shape),
-        ))
-        dataset = DisentDataset(dataset=data, sampler=SingleSampler(), transform=ToImgTensorF32())
-        return metric_mig(dataset, lambda x: module.encode(x), num_train=200)['mig.discrete_score']
+def test_compute_dci(unwrapped_env_data, wrapped_env_data):
+    def get_dci(data):
+        dataset = DisentDataset(dataset=data, sampler=SingleSampler())
+        return metric_dci(dataset, lambda x: x, num_train=100, num_test=50)['dci.disentanglement']
 
-    disentangled_mig_score = get_mig(unwrapped_env_data)
-    entangled_mig_score = get_mig(wrapped_env_data)
-    assert entangled_mig_score < disentangled_mig_score
+    disentangled_dci_score = get_dci(unwrapped_env_data)
+    entangled_dci_score = get_dci(wrapped_env_data)
+    assert entangled_dci_score < disentangled_dci_score
+    print(f'disentangled: {disentangled_dci_score}')
+    print(f'entangled: {entangled_dci_score}')
