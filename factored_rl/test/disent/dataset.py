6ed0e795eecf1e8c1f8d5f68c0f98de386533598
Cam Allen <csal@brown.edu>
2022-09-11 19:57:56 -0400

    Add mig test & update [submodule] visgrid: hidden_goal+is_valid_pos

--- factored_rl/test/disent/dataset.py
+++ factored_rl/test/disent/dataset.py
@@ -7,7 +7,7 @@ from disent.dataset.data import GymEnvData
 from disent.dataset import DisentDataset
 
 from disent.dataset.sampling import SingleSampler
-from disent.metrics import metric_dci
+from disent.metrics import metric_dci, metric_mig
 
 from visgrid.envs.components import Grid
 from visgrid.envs import GridworldEnv
@@ -68,6 +68,17 @@ def test_compute_dci(unwrapped_env_data, wrapped_env_data):
     assert entangled_dci_score < disentangled_dci_score
     print(f'disentangled: {disentangled_dci_score}')
     print(f'entangled: {entangled_dci_score}')
+
+def test_compute_mig(unwrapped_env_data, wrapped_env_data):
+    def get_mig(data):
+        dataset = DisentDataset(dataset=data, sampler=SingleSampler())
+        return metric_mig(dataset, lambda x: x, num_train=100)['mig.discrete_score']
+
+    disentangled_mig_score = get_mig(unwrapped_env_data)
+    entangled_mig_score = get_mig(wrapped_env_data)
+    assert entangled_mig_score < disentangled_mig_score
+    print(f'disentangled: {disentangled_mig_score}')
+    print(f'entangled: {entangled_mig_score}')
 
 def test_gymenvdata_protocol_checking():
     class FakeGymEnv():
