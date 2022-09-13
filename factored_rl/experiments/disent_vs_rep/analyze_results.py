01443f99385a1b740ab3a8ba4d9e6322feeb6a1c
Cam Allen <csal@brown.edu>
2022-09-13 16:01:57 -0400

    Save disent_vs_rep figures

--- factored_rl/experiments/disent_vs_rep/analyze_results.py
+++ factored_rl/experiments/disent_vs_rep/analyze_results.py
@@ -36,10 +36,13 @@ for env in ['gridworld', 'taxi']:
                 x='metric',
                 hue='transform',
                 hue_order=['identity', 'permute_factors', 'permute_states', 'rotate', 'images'],
-                palette='colorblind')
+                # palette='colorblind',
+                )
     plt.title(f'Metric score vs. representation transform ({env})')
     plt.xlabel('Metric')
     plt.ylabel('Score')
+    plt.savefig(f'images/disent_vs_rep/{env}.png', facecolor='white')
+    plt.tight_layout()
     plt.show()
 
 #%%
