28b831f4e4e3a61e541ad18e67c6cf002b23adc1
Cam Allen <csal@brown.edu>
2022-09-13 18:46:20 -0400

    Implement remainder of rl_vs_rep experiment

--- factored_rl/experiments/rl_vs_rep/run.py
+++ factored_rl/experiments/rl_vs_rep/run.py
@@ -1,23 +1,21 @@
+import json
+import yaml
+
 # Args & hyperparams
-from dataclasses import dataclass
-import hydra
 from factored_rl import configs
-
-# Filesystem & devices
-import torch
-import logging
-
-# Agent
-from factored_rl.agents.dqn import DQNAgent
 
 # Env
 import gym
-from gym.wrappers.flatten_observation import FlattenObservation
+from gym.wrappers import FlattenObservation, TimeLimit
 import numpy as np
 from visgrid.envs import GridworldEnv, TaxiEnv
 from factored_rl.wrappers import RotationWrapper
 from factored_rl.wrappers import FactorPermutationWrapper, ObservationPermutationWrapper
 from visgrid.wrappers import GrayscaleWrapper, InvertWrapper, FloatWrapper, NormalizeWrapper, NoiseWrapper, TransformWrapper
+
+# Agent
+from factored_rl.agents.dqn import DQNAgent
+from tqdm import tqdm
 
 # ----------------------------------------
 # Args & hyperparameters
@@ -25,14 +23,12 @@ from visgrid.wrappers import GrayscaleWrapper, InvertWrapper, FloatWrapper, Norm
 
 parser = configs.new_parser()
 # yapf: disable
-parser.add_argument('-e', '--experiment', type=str, default='rl_vs_disent', help='A name for the experiment')
+parser.add_argument('-e', '--experiment', type=str, default='rl_vs_rep', help='A name for the experiment')
 parser.add_argument('-t', '--trial', type=str, default='trial', help='A name for the trial')
 parser.add_argument('-s', '--seed', type=int, default=0, help='A seed for the random number generator')
 parser.add_argument('--no-timestamp', action='store_true', help='Disable automatic trial timestamps')
 parser.add_argument('--noise', action='store_true')
-parser.add_argument('--rotate', action='store_true')
-parser.add_argument('--permute', type=str, default=None, choices=[None, 'factors', 'states'])
-parser.add_argument('--images', action='store_true')
+parser.add_argument('--transform', type=str, default='identity', choices=['identity', 'images', 'permute_factors', 'permute_states', 'rotate'])
 parser.add_argument('--test', action='store_true')
 parser.add_argument('-f', '--fool-ipython', action='store_true',
     help='Dummy arg to make ipython happy')
@@ -79,6 +75,8 @@ def initialize_env(args, env_cfg: configs.EnvConfig):
             env = TransformWrapper(RotationWrapper(env), lambda x: x / np.sqrt(2))
     if args.noise:
         env = NoiseWrapper(env, env_cfg.noise_std)
+
+    env = TimeLimit(env, max_episode_steps=env_cfg.n_steps_per_episode)
     return env
 
 # ----------------------------------------
@@ -89,18 +87,64 @@ def initialize_agent(env, args, cfg: configs.AgentConfig):
     agent = DQNAgent(env.observation_space, env.action_space, cfg)
     return agent
 
+# ----------------------------------------
+# Evaluate RL performance
+# ----------------------------------------
+def train_agent_on_env(agent, env, n_episodes, results_file=None):
+    total_reward = 0
+    total_steps = 0
+    losses = []
+    for episode in tqdm(range(n_episodes), desc='episodes'):
+        ob, info = env.reset()
+        terminal, truncated = False, False
+        ep_rewards = []
+        ep_steps = 0
+        while not (terminal or truncated):
+            action = agent.act(ob)
+            next_ob, reward, terminal, truncated, info = env.step(action)
+            experience = {
+                'ob': ob,
+                'action': action,
+                'reward': reward,
+                'terminal': terminal,
+                'truncated': truncated,
+                'next_ob': next_ob,
+            }
+            agent.store(experience)
+
+            loss = agent.update()
+            losses.append(loss)
+            ep_rewards.append(reward)
+            ep_steps += 1
+            ob = next_ob
+            if terminal or truncated:
+                break
+
+        total_steps += ep_steps
+        total_reward += sum(ep_rewards)
+        episode_result = {
+            'episode': episode,
+            'reward': sum(ep_rewards),
+            'steps': ep_steps,
+            'total_reward': total_reward,
+            'total_steps': total_steps,
+        }
+        if results_file is not None:
+            json_str = json.dumps(episode_result)
+            results_file.write(json_str + '\n')
+            results_file.flush()
+        log.info('\n' + yaml.dump(episode_result, sort_keys=False))
+
+# ----------------------------------------
+# Run experiment
+# ----------------------------------------
+
 args, cfg, log = configs.initialize_experiment(parser)
 env = initialize_env(args, cfg.env)
 agent = initialize_agent(env, args, cfg.agent)
 
-ob, _ = env.reset()
-if args.images:
-    env.plot(ob)
-else:
-    print(ob)
-
-# ----------------------------------------
-# RL performance for representation
-# ----------------------------------------
-
-# TODO
+filename = cfg.experiment.dir + 'results.json'
+with open(filename, 'w') as results_file:
+    results_file.write('[\n')
+    train_agent_on_env(agent, env, cfg.env.n_training_episodes, results_file)
+    results_file.write(']\n')
