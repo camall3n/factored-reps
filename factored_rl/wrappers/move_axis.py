fabb907db068e4526f7f51057874aa3fa9c41218
Cam Allen <csal@brown.edu>
2022-09-25 14:12:45 -0400

    Fix bug with MoveAxisWrapper for Box observations

--- factored_rl/wrappers/move_axis.py
+++ factored_rl/wrappers/move_axis.py
@@ -1,6 +1,7 @@
 import copy
 import numpy as np
 import gym
+import gym.spaces
 
 class MoveAxisWrapper(gym.ObservationWrapper):
     """
@@ -13,8 +14,12 @@ class MoveAxisWrapper(gym.ObservationWrapper):
 
         example_obs = np.empty(env.observation_space.shape)
         new_shape = np.moveaxis(example_obs, source, destination).shape
-        self.observation_space = copy.deepcopy(env.observation_space)
-        self.observation_space._shape = new_shape
+        ob_space = copy.deepcopy(env.observation_space)
+        ob_space._shape = new_shape
+        if isinstance(ob_space, gym.spaces.Box):
+            ob_space.low = np.moveaxis(ob_space.low, source, destination)
+            ob_space.high = np.moveaxis(ob_space.high, source, destination)
+        self.observation_space = ob_space
 
     def observation(self, obs):
         obs = np.moveaxis(obs, self.source, self.destination)
