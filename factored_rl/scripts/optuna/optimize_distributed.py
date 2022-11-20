37eabe721a925fb9af12ab8170bb639828e40fd8
Cam Allen <csal@brown.edu>
2022-11-19 19:21:35 -0500

    Add --summary arg for optuna script

--- factored_rl/scripts/optuna/optimize_distributed.py
+++ factored_rl/scripts/optuna/optimize_distributed.py
@@ -1,3 +1,4 @@
+import argparse
 import optuna
 
 def objective(trial):
@@ -5,8 +6,19 @@ def objective(trial):
     return (x - 2)**2
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--worker', type=str)
+    parser.add_argument('--summary', action='store_true')
+    args = parser.parse_args()
     study = optuna.create_study(
         study_name="distributed-example",
         storage="sqlite:///factored_rl/scripts/optuna/distributed-example.db",
         load_if_exists=True)
-    study.optimize(objective, n_trials=10)
+
+    if not args.summary:
+        study.optimize(objective, n_trials=10)
+    else:
+        print(f'Best param: {study.best_params}')
+        print(f'Best value: {study.best_value}')
+        print()
+        print(f'Best trial: {study.best_trials}')
