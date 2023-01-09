966af9b1e1e5be93e72c736b774baa983712bd7e
Shreyas Raman <shreyas_sundara_raman@brown.edu>
2023-01-09 06:38:17 -0500

    updated env to account for any gym: classic control and box2d envs

--- pendulum_env.py
+++ pendulum_env.py
@@ -9,50 +9,60 @@ import matplotlib.pyplot as plt
 import numpy as np
 import copy
 import pdb
-import os 
 
-class GymEnv(gym.Env):
 
-    HAS_HIDDEN_GOAL = set([])
+# from .. import utils
 
-    def __init__(self, exploring_starts: bool = True, start_position: Tuple = None, hidden_goal: bool = False, terminate_on_goal: bool = True, init_state: Tuple = None,should_render: bool = True, env_name = 'Pendulum-v1', init_kwargs = {}, goal_state = None, gym_rewards=True):
-        
-        kwargs = {'render_mode':'rgb_array'}
-        for (k,v) in init_kwargs.items():
-            kwargs[k] = v
 
-        self.environment = gym.make(env_name, **kwargs)
+class PendulumEnv(gym.Env):
 
+    _gravity_const = 9.81
+    
+
+    def __init__(self, exploring_starts: bool = True, start_position: Tuple = None,
+                 terminate_on_goal: bool = True,
+                 init_state: Tuple = None,
+                 should_render: bool = True):
+    
+
+        """
+        Pendulum Environment: Gym Wrapper
+
+        Notes:
+            removed fixed_goal argument since goal always fixed in pendulum
+
+            dimensions now represents the render size
+        """
+
+        self.environment = gym.make('Pendulum-v1', g= self._gravity_const)
         self._initial_state = None
 
         self.exploring_starts = exploring_starts if start_position is None else False
-
         self.fixed_goal = True
-
-        if env_name in self.HAS_HIDDEN_GOAL:
-            self.hidden_goal = hidden_goal
-        
+        #self.hidden_goal = hidden_goal
         self.terminate_on_goal = terminate_on_goal
         self.should_render = should_render
         
 
         self.action_space = self.environment.action_space
-        self.goal_state = goal_state
-        self.use_gym_rewards = gym_rewards
-
+        self.goal_state = np.array([0.0, 0.0, 0.0])
         self.current_state = None
 
         self._initialize_env_state(init_state)
         self._initialize_state_space()
         self._initialize_obs_space()
+
     
     def _initialize_state_space(self):
         
-        #state represented as the internal observation_space of the environment
-        self.state_space = self.environment.observation_space
+        #state represented as: (x, y, angular vel) with ranges ([-1,1], [-1,1], [-8,8])
+        #TODO: self.environment.state missing the angular velocity so left as the x,y coord
+        self.state_space = spaces.Box(low=np.array([-1,-1,-8]), high=np.array([1,1,8]))
+
     
     def _initialize_obs_space(self):
         
+        #TODO: Do we need mode = 'rgb_array' because it works without?
         env_screen_shape = self.environment.render().shape
 
         self.img_observation_space = spaces.Box(0.0, 1.0, env_screen_shape, dtype = np.float32)
@@ -61,7 +71,8 @@ class GymEnv(gym.Env):
         self.factor_observation_space = copy.deepcopy(self.state_space)
 
         self.set_rendering(self.should_render)
-    
+        
+
     def _initialize_env_state(self, init_state = None):
 
         if init_state is not None:
@@ -69,6 +80,7 @@ class GymEnv(gym.Env):
             self._initial_state = init_state
         else:
             self.environment.reset()
+
     
     def _reset(self, init_state):
         self._initial_state = init_state
@@ -81,12 +93,14 @@ class GymEnv(gym.Env):
         self._cached_state = None
         self._cached_render = None
 
+        #TODO: I use the private _reset() function to update the self._initial_state reference
+        # state = self.get_state()
         self._reset(new_obs)
         
         obs = self.get_observation(new_obs)
         info = self._get_info(new_obs)
         return obs, info
-    
+
     def step(self, action: ActType) -> Tuple[ObsType, float, bool, bool, dict]:
         
         self._cached_state = None
@@ -97,53 +111,50 @@ class GymEnv(gym.Env):
         
         self.current_state = state
         
-        if self.goal_state is not None:
-            if self.terminate_on_goal and self._check_goal(state):
-                terminated = True
-            else:
-                terminated = False
+        if self.terminate_on_goal and self._check_goal(state):
+            terminated = True
         else:
-            terminated = gym_terminated
+            terminated = False
         
-        if self.use_gym_rewards:
-            reward = gym_reward
-        else:
-            reward = 1 if terminated else 0
-
+        reward = 1 if terminated else 0
         obs = self.get_observation(state)
         info = self._get_info(state)
 
-        if terminated:
-            self.reset()
-
         return obs, reward, terminated, truncated, info
-    
+
+
     def _step(self, action):
         obs, reward, terminated, truncated, info = self.environment.step(action)
         return obs, reward, terminated, truncated, info
     
     def can_run(self, action):
 
-        if type(self.action_space) is gym.spaces.box.Box and type(action) is not np.ndarray:
-            action = np.asarray([action], dtype=np.float32) if type(action) is not list else np.asarray(action, dtype=np.float32)
-        elif type(self.action_space) is gym.spaces.discrete.Discrete:
-            assert type(action) is int, 'For discrete action space, input needs to be an integer'
-            
-
-        #NOTE: input is either numpy array or list containing 'action' input
-        return True if self.action_space.contains(action) else False
+        return True if (action >= -2.0 and action <= +2.0) else False
     
     def get_state(self):
+
+        # theta, angular_vel = self.environment.state[0], self.environment.state[1]
+        
+        # return np.asarray([np.cos(theta), np.sin(theta), angular_vel])
+
         return self.current_state
+       
+    
     
     def set_state(self, state):
 
+        #NOTE: state property in self.environment is tuple (theta, angular velocity)
+        
         is_valid = self._check_valid_state(state)
 
         assert is_valid, 'Attempted to call set_state with an invalid state'
 
-        self.environment.state = np.array(state)
+        #convert state to theta, angular vel form for self.environment internal state
+        x, y, angular_vel = state[0], state[1], state[2]
 
+        theta = np.arctan2(y,x)
+
+        self.environment.state = np.array([theta, angular_vel])
         self._initial_state = state
         self.current_state = state
     
@@ -184,6 +195,7 @@ class GymEnv(gym.Env):
 
         finally:
             self.set_state(current_state)
+        
     
     def _get_info(self, state=None):
         if state is None:
@@ -196,9 +208,13 @@ class GymEnv(gym.Env):
         if state is None:
             state = self.get_state()
         
-        return self.goal_state(state)
-    
+        for i, val in enumerate(state):
 
+            if val != self.goal_state[i]:
+                return False
+        
+        return True
+    
     def set_rendering(self, enabled=True):
 
         self.should_render = enabled
@@ -230,13 +246,13 @@ class GymEnv(gym.Env):
         
         plt.cla()
         plt.clf()
-        
-    
 
-def test_file(env_name, valid_actions, invalid_actions, init_kwargs={}, goal_state=None):
+
+
+def test_file():
 
     #creating the environment
-    test_env = GymEnv(env_name=env_name,init_kwargs=init_kwargs, goal_state=goal_state)
+    test_env = PendulumEnv()
 
     
 
@@ -253,11 +269,10 @@ def test_file(env_name, valid_actions, invalid_actions, init_kwargs={}, goal_sta
     except Exception as e:
         print('Error: {}'.format(e))
     
-    for x in valid_actions:
-        assert test_env.can_run(x) is True
     
-    for x in invalid_actions:
-        assert test_env.can_run(x) is False
+    assert test_env.can_run(1.8234) is True
+    assert test_env.can_run(-10.01) is False
+    assert test_env.can_run(2.0000000001) is False
 
     
     curr_state = test_env.get_state()
@@ -268,27 +283,20 @@ def test_file(env_name, valid_actions, invalid_actions, init_kwargs={}, goal_sta
 
     new_action = test_env.action_space.sample()
     print('Sampled Action: {}'.format(new_action))
-
-    if not os.path.isdir(os.path.relpath('./test/{}'.format(env_name))):
-        os.mkdir('./test/{}'.format(env_name))
-
     
-    test_env.plot(blocking=False, save=True, filename='./test/{}/old_obs.png'.format(env_name))
+    test_env.plot(blocking=False, save=True, filename='./test/old_obs.png')
 
     
     obs, reward, terminated, truncated, info = test_env.step(new_action)
     print('New Post-Action State (get_state): ', test_env.get_state())
 
-    test_env.plot(blocking=False, save=True, filename='./test/{}/action_applied.png'.format(env_name))
-
-    new_action = test_env.action_space.sample()
-    print('New Sampled Action: {}'.format(new_action))
+    test_env.plot(blocking=False, save=True, filename='./test/action_applied.png')
 
     for i in range(10):
-        obs, reward, terminated, truncated, info = test_env.step(new_action)
+        obs, reward, terminated, truncated, info = test_env.step(np.array([0.0]))
 
         if i == 3 or i==5 or i==7:
-            test_env.plot(blocking=False, save=True, filename='./test/{}/step_{}.png'.format(env_name, i))
+            test_env.plot(blocking=False, save=True, filename='./test/step_{}.png'.format(i))
 
     print('New State (get_state): ', test_env.get_state())
 
@@ -297,33 +305,25 @@ def test_file(env_name, valid_actions, invalid_actions, init_kwargs={}, goal_sta
     assert test_env.is_valid_state(curr_state) is True
     assert test_env.is_valid_state(None) is False
 
-    test_env.plot(blocking=False, save=True, filename='./test/{}/final_obs.png'.format(env_name))
+    test_env.plot(blocking=False, save=True, filename='./test/final_obs.png')
 
     test_env.set_state(curr_state)
     print('Reset State (get_state): ', test_env.get_state())
 
-    test_env.plot(current_obs, blocking=False, save=True, filename='./test/{}/old_obs_arg.png'.format(env_name))
+    
 
+    test_env.plot(current_obs, blocking=False, save=True, filename='./test/old_obs_arg.png')
+
+
+    
 
 
 should_test = True
 if __name__=='__main__':
 
     if should_test:
-        pdb.set_trace()
-        test_file('Pendulum-v1', init_kwargs={'g':9.81}, goal_state = lambda x: x[0]==0.0 and x[1]==0.0 and x[2]==0.0, valid_actions = [0.000001, 1.8234], invalid_actions = [-10.01, 2.000001])
-
-        test_file('CartPole-v1', valid_actions = [0,1], invalid_actions = [-1,5])
-
-        test_file('MountainCar-v0', goal_state = lambda x: x[0]>=0.5, valid_actions = [2,0,1], invalid_actions = [-1, 3, 4, 5])
-
-        test_file('Acrobot-v1', goal_state = lambda x: -x[0]-(x[0]*x[2]-x[1]*x[3]) > 1.0, valid_actions = [2,0,1], invalid_actions = [-1, -4, 3,5])
-
-        test_file('BipedalWalker-v3', init_kwargs={'hardcore':False}, valid_actions = [[-0.99,+0.9,-0.5,+0.4]], invalid_actions = [[-1.1,0.5,-0.9,0.8], [1.1, 2.00001,5,-1.00001]])
-
-
+        test_file()
     
 
-
-
+    
 
